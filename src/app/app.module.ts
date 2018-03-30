914628d93c1655a3188088ef7c4eaae6e014d73a
Guest User <Guest@Epicodus-3D.local>
2018-03-30 08:59:31 -0700

    add topnav component and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
+
+import {SuiModule} from 'ng2-semantic-ui';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent
   ],
   imports: [
+    SuiModule,
     BrowserModule
   ],
   providers: [],
